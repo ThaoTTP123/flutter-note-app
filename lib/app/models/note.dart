00429846a9b9c2ffc73e4bf1306283f702546d95
ThaoTTP <dreemmurrchara@gmail.com>
2025-09-10 11:38:34 +0700

    Initial commit - Notes App using Nylo

---
+++ lib/app/models/note.dart
@@ -0,0 +1,39 @@
+import 'package:nylo_framework/nylo_framework.dart';
+
+class Note extends Model {
+  static StorageKey key = "note";
+  int? id;
+  String title;
+  String content;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  Note({
+    this.id,
+    required this.title,
+    this.content = '',
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  })  : createdAt = createdAt ?? DateTime.now(),
+        updatedAt = updatedAt ?? DateTime.now(),
+        super(key: key);
+
+  Note.fromJson(data)
+      : id = data['id'],
+        title = data['title'] ?? '',
+        content = data['content'] ?? '',
+        createdAt = DateTime.parse(data['createdAt']),
+        updatedAt = DateTime.parse(data['updatedAt']),
+        super(key: key) {}
+
+  @override
+  toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+}
