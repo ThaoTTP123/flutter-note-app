fe55a1ff1fffbb422bd07b48b19af274cb871239
ThaoTTP <dreemmurrchara@gmail.com>
2025-09-12 18:25:00 +0700

    create note card widget

---
+++ lib/resources/widgets/note_card_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/app/models/note.dart';
+
+class NoteCard extends StatelessWidget {
+  final Note note;
+  final VoidCallback onTap;
+
+  const NoteCard({
+    super.key,
+    required this.note,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                note.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Expanded(
+                child: Text(
+                  note.content,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 5,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
